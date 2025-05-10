6f1730260359820e5cb1cb6fa4f87ad9c92f1e13
qordlfrb119 <105373234+qordlfrb119@users.noreply.github.com>
2025-05-10 22:35:11 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const OpenAI = require("openai");
 
 const app = express();
 app.use(cors({
-  origin: 'https://start-now-chat.vercel.app/', 
+  origin: 'https://start-now-chat.vercel.app', 
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
